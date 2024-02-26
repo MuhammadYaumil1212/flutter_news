cf11d9e80f6445e262932410a5b4291d87a8e913
Muhammad Yaumil  Ramadhani <muhammadyaumilramadhani@gmail.com>
2024-02-26 10:01:04 +0700

    Fix bug api service

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,4 +1,4 @@
 const String baseUrl = "https://newsapi.org/v2/";
-const String newsApikey = "";
+const String newsApikey = "5b766c23e6f24297b5007f0b9eb45ab9";
 const String countryKey = "us";
 const String categoryQuery = "general";
